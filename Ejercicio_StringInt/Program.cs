44b2875e474cef43efe183e306177c8cf336afe3
Pedro Ricardo <pedrochavez645@live.com>
2021-03-02 19:29:28 -0800

    se inserto un nombre y edad, y se imprimio con el mensaje de "Hola tu nombre es: y tu edad es:"

---
+++ Ejercicio_StringInt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_StringInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int num;
+            Console.WriteLine("Escribe tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola {0}, cuantos años tienes?", nombre);
+            num = int.Parse(System.Console.ReadLine());
+
+            Console.WriteLine("Hola tu nombre es: " + nombre + " y tu edad es: " + num);
+            Console.WriteLine("Presiona cualquier tecla para continuar");
+            Console.ReadKey();
+
+        }
+    }
+}
